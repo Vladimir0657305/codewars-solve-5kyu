cddce2f474e32ae81ffef84043662710813f0f6f
Vladimir0657305 <vv0657305@gmail.com>
2023-01-26 12:19:47 +0200

    changes

--- script.js
+++ script.js
@@ -90,3 +90,18 @@ function pigIt(str) {
 // =====================================
 // return str.split(" ").map((item) => `${item.substr(1)}${item[0]}ay`).join(" ")
 // =====================================
+function removeSmallest(numbers) {
+    let min = numbers[0];
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] < min) {
+            min = numbers[i];
+        }
+    }
+    let arrTemp = [...numbers];
+    // console.log(arrTemp);
+    arrTemp.splice(arrTemp.indexOf(min), 1);
+
+    // console.log(arrTemp);
+    // console.log(min);
+}
+removeSmallest(arr);
